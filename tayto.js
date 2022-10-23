852cf04fd346559b6ccf1ddb702cfd704f7f1f7f
mim <mim@hotmail.co.uk>
2022-10-23 17:55:22 +0100

    Add hours for each weekday

--- tayto.js
+++ tayto.js
@@ -2,23 +2,27 @@
 window.localStorage.tayto = window.localStorage.tayto || JSON.stringify({ 'clients': {} });
 const tayto = JSON.parse(window.localStorage.tayto);
 
-refresh_client_list();
 open_client(tayto.activeClient);
+
+function refresh() {
+    refresh_client_list();
+    refresh_lines();
+}
 
 function add_new_line() {
     tayto.clients[tayto.activeClient].push({
-        description : "",
-        person : "",
-        monday: 0,
+        description: "",
+        person: "",
+        hours: [0, 0, 0, 0, 0, 0, 0],
     });
     save();
-    refresh_lines();
+    refresh();
 }
 
 function delete_line(index) {
     tayto.clients[tayto.activeClient].splice(index, 1);
     save();
-    refresh_lines();
+    refresh();
 }
 
 function refresh_lines() {
@@ -27,10 +31,16 @@ function refresh_lines() {
 
     for (const [i, line] of tayto.clients[tayto.activeClient].entries()) {
         linesDiv.innerHTML += `
-        <div>
-          <input type="text"   value="${line.description}" oninput="update_lines()" />
-          <input type="text"   value="${line.person}"      oninput="update_lines()" />
-          <input type="number" value="${line.monday}"      oninput="update_lines()" />
+        <div class="line">
+          <input class="line_descn"  type="text"   value="${line.description}"                         oninput="update_lines()" />
+          <input class="line_person" type="text"   value="${line.person}"                              oninput="update_lines()" />
+          <input class="line_hours"  type="number" value="${line.hours[0]}"    onClick="this.select()" oninput="update_lines()" />
+          <input class="line_hours"  type="number" value="${line.hours[1]}"    onClick="this.select()" oninput="update_lines()" />
+          <input class="line_hours"  type="number" value="${line.hours[2]}"    onClick="this.select()" oninput="update_lines()" />
+          <input class="line_hours"  type="number" value="${line.hours[3]}"    onClick="this.select()" oninput="update_lines()" />
+          <input class="line_hours"  type="number" value="${line.hours[4]}"    onClick="this.select()" oninput="update_lines()" />
+          <input class="line_hours"  type="number" value="${line.hours[5]}"    onClick="this.select()" oninput="update_lines()" />
+          <input class="line_hours"  type="number" value="${line.hours[6]}"    onClick="this.select()" oninput="update_lines()" />
           <button onclick="delete_line(${i})">X</button>
         </div>
       `;
@@ -42,8 +52,14 @@ function update_lines() {
     for (let index = 0; index < linesDiv.children.length; index++) {
         const line = linesDiv.children[index];
         tayto.clients[tayto.activeClient][index].description = line.children[0].value;
-        tayto.clients[tayto.activeClient][index].person      = line.children[1].value;
-        tayto.clients[tayto.activeClient][index].monday      = line.children[2].value;
+        tayto.clients[tayto.activeClient][index].person = line.children[1].value;
+        tayto.clients[tayto.activeClient][index].hours[0] = line.children[2].value;
+        tayto.clients[tayto.activeClient][index].hours[1] = line.children[3].value;
+        tayto.clients[tayto.activeClient][index].hours[2] = line.children[4].value;
+        tayto.clients[tayto.activeClient][index].hours[3] = line.children[5].value;
+        tayto.clients[tayto.activeClient][index].hours[4] = line.children[6].value;
+        tayto.clients[tayto.activeClient][index].hours[5] = line.children[7].value;
+        tayto.clients[tayto.activeClient][index].hours[6] = line.children[8].value;
     }
     save();
 }
@@ -52,8 +68,7 @@ function open_client(id) {
     document.getElementById("selected_client").innerText = id;
     tayto.activeClient = id;
     save();
-    refresh_client_list();
-    refresh_lines();
+    refresh();
 }
 
 function refresh_client_list() {
@@ -86,14 +101,14 @@ function add_new_client() {
     if (newClient in tayto.clients == false) tayto.clients[newClient] = [];
     open_client(newClient);
     save();
-    refresh_client_list();
     close_add_new_client_window();
+    refresh();
 }
 
 function remove_client(id) {
     delete tayto.clients[id];
     save();
-    refresh_client_list();
+    refresh();
 }
 
 function save() {
